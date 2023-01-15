7a3e77219313d7c64aa42106e6cc03b2000d4c81
BomberQuFest <93448868+BomberQuFest@users.noreply.github.com>
2023-01-15 21:56:30 +0300

    Добавьте файлы проекта.

---
+++ ProjectManager/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectManager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ProjectOne.Calendar.Main();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ProjectTwo.Form1 form1 = new ProjectTwo.Form1();
+            form1.Show();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+         
+        }
+    }
+}
---
+++ ProjectOne/Calendar.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Forms;
+namespace ProjectOne
+{
+    public class Calendar
+    {
+        public static void Main()
+        {
+            DateTime now = GetCurrentDate();
+            MessageBox.Show($"Today's date is {now}");
+        }
+        static DateTime GetCurrentDate() => DateTime.Now.Date;
+    }
+}
+
+    
